f647f409e9122ad1c5c4fefe88e64c075115ea36
Nimesh Kavinda <44240093+nimeshkavinda@users.noreply.github.com>
2021-03-17 00:02:54 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,12 +6,12 @@ import bodyParser from "body-parser";
 const featuredEvent = [
   {
     id: 1,
-    name: "Google Crowdsource",
-    date: "Thursday, February 25, 2021",
-    time: "6:00 PM to 3:00 AM GMT+5:30",
+    name: "Google Crowdsource 2021",
+    date: "Wednesday, March 17, 2021",
+    time: "10:00 AM to 12:00 PM GMT+5:30",
     venue: "FOC C2-002, NSBM Green University",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.",
+      "Crowdsource is a crowdsourcing platform developed by Google intended to improve a host of Google services through the user-facing training of different algorithms. Crowdsource was released for Android on the Google Play store on August 29, 2016, and is also available on the web.",
     imageUrl: "/assets/1.jpg",
     rsvpUrl: "",
   },
