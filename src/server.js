12c3a8d3c25a0e185399179ac506ef4ba4307d1a
Nimesh Kavinda <44240093+nimeshkavinda@users.noreply.github.com>
2021-03-20 15:11:24 +0530

    event params get

--- src/server.js
+++ src/server.js
@@ -1,116 +1,47 @@
 import express from "express";
 import bodyParser from "body-parser";
-// import upcomingEvents from "./data";
-// import pastEvents from "./data";
-
-const featuredEvent = [
-  {
-    id: 1,
-    name: "Google Crowdsource 2021",
-    date: "Wednesday, March 17, 2021",
-    time: "10:00 AM to 12:00 PM GMT+5:30",
-    venue: "FOC C2-002, NSBM Green University",
-    description:
-      "Crowdsource is a crowdsourcing platform developed by Google intended to improve a host of Google services through the user-facing training of different algorithms. Crowdsource was released for Android on the Google Play store on August 29, 2016, and is also available on the web.",
-    imageUrl: "/assets/1.jpg",
-    rsvpUrl: "https://fossnsbm.org/events/",
-  },
-];
-
-const upcomingEvents = [
-  {
-    id: 1,
-    name: "Google Crowdsource",
-    date: "Thursday, February 25, 2021",
-    time: "6:00 PM to 3:00 AM GMT+5:30",
-    venue: "FOC C2-002, NSBM Green University",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.",
-    imageUrl: "/assets/1.jpg",
-    rsvpUrl: "",
-  },
-  {
-    id: 2,
-    name: "ExpressJS Workshop",
-    date: "Thursday, February 25, 2021",
-    time: "6:00 PM to 3:00 AM GMT+5:30",
-    venue: "FOC C2-002, NSBM Green University",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.",
-    imageUrl: "/assets/2.jpeg",
-    rsvpUrl: "",
-  },
-  {
-    id: 3,
-    name: "Google Crowdsource",
-    date: "Thursday, February 25, 2021",
-    time: "6:00 PM to 3:00 AM GMT+5:30",
-    venue: "FOC C2-002, NSBM Green University",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.",
-    imageUrl: "/assets/3.jpg",
-    rsvpUrl: "",
-  },
-];
-
-const pastEvents = [
-  {
-    id: 1,
-    name: "Hash Code",
-    date: "Thursday, February 25, 2021",
-    time: "6:00 PM to 3:00 AM GMT+5:30",
-    venue: "FOC C2-002, NSBM Green University",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.",
-    imageUrl: "/images/4.jpg",
-    rsvpUrl: "",
-  },
-  {
-    id: 2,
-    name: "SFD 2020",
-    date: "Thursday, February 25, 2021",
-    time: "6:00 PM to 3:00 AM GMT+5:30",
-    venue: "FOC C2-002, NSBM Green University",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.",
-    imageUrl: "/images/5.jpg",
-    rsvpUrl: "",
-  },
-  {
-    id: 3,
-    name: "Mozilla Monthly Meetup",
-    date: "Thursday, February 25, 2021",
-    time: "6:00 PM to 3:00 AM GMT+5:30",
-    venue: "FOC C2-002, NSBM Green University",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.",
-    imageUrl: "/images/6.jpg",
-    rsvpUrl: "",
-  },
-];
+import { MongoClient } from "mongodb";
 
 const app = express();
-
-// const db = require('./models/mongo');
+// const db = require("../mongo");
 
 // db.connect(() => {
 //   app.listen(process.env.PORT || 5555, function () {
-//     console.log(`Listening`);
+//     console.log("mongo client is connected");
 //   });
 // });
+
+app.listen(8000, () => {
+  console.log("server is listening");
+});
 
 app.get("/test", (req, res) => {
   res.send("testing");
 });
 
-app.get("/api/events", (req, res) => {
-  res.status(200).json(featuredEvent);
+app.get("/api/events", async (req, res) => {
+  const client = await MongoClient.connect("mongodb://127.0.0.1:27017", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  const db = client.db("foss-rsvp");
+  const events = await db.collection("events").find({}).toArray();
+  res.status(200).json(events);
+  client.close();
 });
 
-app.get("/api/pastevents", (req, res) => {
-  res.status(200).json(pastEvents);
-});
-
-app.listen(8000, () => {
-  console.log("server is listening");
+app.get("/api/events/:eventId", async (req, res) => {
+  const { eventId } = req.params;
+  const client = await MongoClient.connect("mongodb://127.0.0.1:27017", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  const db = client.db("foss-rsvp");
+  const event = await db.collection("events").findOne({ id: eventId });
+  if (event) {
+    res.status(200).json(event);
+  } else {
+    res.status(404).json("Not found");
+  }
+  client.close();
 });
