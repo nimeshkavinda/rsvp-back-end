6334aec2043a6baac9c164b1f8c2e49b6d2f9f50
Nimesh Kavinda <44240093+nimeshkavinda@users.noreply.github.com>
2021-03-29 17:54:51 +0530

    serve frontend build

--- src/server.js
+++ src/server.js
@@ -1,15 +1,23 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { MongoClient } from "mongodb";
+import history from "connect-history-api-fallback";
 
 const app = express();
-// const db = require("../mongo");
+// const mongo = require("../mongo");
 
-// db.connect(() => {
+// mongo.connect(() => {
 //   app.listen(process.env.PORT || 5555, function () {
 //     console.log("mongo client is connected");
 //   });
 // });
+app.use(
+  express.static(path.resolve(__dirname, "../dist"), {
+    maxAge: "1y",
+    eTag: false,
+  })
+);
+app.use(history());
 
 app.listen(8000, () => {
   console.log("server is listening");
@@ -37,11 +45,32 @@ app.get("/api/events/:eventId", async (req, res) => {
     useUnifiedTopology: true,
   });
   const db = client.db("foss-rsvp");
-  const event = await db.collection("events").findOne({ id: eventId });
+  const event = await db.collection("events").findOne({}, { id: eventId });
   if (event) {
     res.status(200).json(event);
   } else {
     res.status(404).json("Not found");
   }
   client.close();
+});
+
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   next();
+// });
+
+// app.get('/fetch', (req, res) => {
+//   request(
+//     { url: req.query.url },
+//     (error, response, body) => {
+//       if (error || response.statusCode !== 200) {
+//         return res.status(500).send('error');
+//       }
+//       res.send(body);
+//     }
+//   )
+// });
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "--/dist/index.html"));
 });
